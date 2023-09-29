87c1bf34b4adfebce8c7ea36ce49591e3f79fba0
matt-culbert <culbertmatt@gmail.com>
2023-09-29 10:41:34 -0400

    futzing with the install script

--- install.sh
+++ install.sh
@@ -1,2 +1,11 @@
+go get github.com/google/uuid
 go build Builder/builder.go
 python3 pip install -r requirements.txt
+
+openssl genrsa -out ca.key 2048 
+
+openssl req -new -x509 -days 365 -key ca.key -subj "/C=CN/ST=GD/L=SZ/O=Acme, Inc./CN=Acme Root CA" -out ca.crt
+
+openssl req -newkey rsa:2048 -nodes -keyout server.key -subj "/C=CN/ST=GD/L=SZ/O=Acme, Inc./CN=_*.example.com" -out server.csr 
+
+openssl x509 -req -extfile <(printf "subjectAltName=DNS:example.com,DNS:www.example.com") -days 365 -in server.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out server.crt
