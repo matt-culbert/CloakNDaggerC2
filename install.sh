f173f8348b9e8d64e7c9714583fc2c0bbc769c02
matt-culbert <culbertmatt@gmail.com>
2024-01-18 14:20:40 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,7 @@
 go mod tidy
 
-go_src_dir="src"
-
-go build "./$go_src_dir/Listeners/controller.go Listeners/listener_func.go"
-go build "./$go_src_dir/API/API_Listener.go"
-go build "./$go_src_dir/Builder/builder.go"
+go build "./Listeners/controller.go Listeners/listener_func.go"
+go build "./API/API_Listener.go"
+go build "./Builder/builder.go"
 
 docker run --name redis -p 6379:6379 -d redis
