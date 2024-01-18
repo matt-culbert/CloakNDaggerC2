a4e32feba988641c19678f3633845f94fe7c6c5b
matt-culbert <culbertmatt@gmail.com>
2024-01-18 14:18:03 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 go mod tidy
 
+go_src_dir="src"
+
 go build "./$go_src_dir/Listeners/controller.go Listeners/listener_func.go"
 go build "./$go_src_dir/API/API_Listener.go"
 go build "./$go_src_dir/Builder/builder.go"
